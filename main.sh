10ff0425e427c0e6c0dcc4f920102c7d5e5a9b5c
Sumer Shende <sam.sumer@gmail.com>
2017-03-15 14:47:17 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,16 @@
-TEST="wget http://35.163.220.202:8080/job/iTrust/ws/tests.json >> tests.json" 
+rm tests.json || true
+TEST="wget http://35.163.220.202:8080/job/iTrust/ws/tests.json >> tests.json"
 echo `$TEST`
 value=($(jq -r '.count' tests.json))
 echo $value
-while [$value < 8]
+while (($value < 8))
 do
-	bash test.sh 
+        bash test.sh
+        rm tests.json
+        TEST="wget http://35.163.220.202:8080/job/iTrust/ws/tests.json >> tests.json"
+        echo `$TEST`
+        value=($(jq -r '.count' tests.json))
+        echo $value
 done
+rm report.txt
 python reportGenerator.py >> report.txt
